64d953cff5b1d0d9c570c32d0ffab94b1e821985
Horea Christian <chr@chymera.eu>
2024-03-11 21:06:09 -0400

    Added tests and testdata, removed trace files

---
+++ nwb2bids/conftest.py
@@ -0,0 +1,32 @@
+import tempfile
+import pynwb
+import numpy
+import pytest
+import os
+from datetime import datetime
+from dateutil import tz
+
+@pytest.fixture(scope="session")
+def nwb_testdata(
+    tmp_path_factory,
+    ):
+    from pynwb.testing.mock.file import mock_NWBFile
+    from pynwb.file import Subject
+
+    nwbfile = mock_NWBFile(
+        session_start_time = datetime(2024, 3, 9, 22, 30, 3, tzinfo=tz.gettz("US/Eastern")),
+        session_id = "20240309",
+        )
+    time_series = pynwb.TimeSeries(name="Test", data=numpy.array([], dtype="uint8"), unit="n.a.", rate=1.0)
+    nwbfile.add_acquisition(time_series)
+
+    subject = Subject(
+        subject_id="1234",
+        sex="male",
+        )
+    nwbfile.subject = subject
+    filename = tmp_path_factory.mktemp("test_nwb2bids") / "testfile.nwb"
+    with pynwb.NWBHDF5IO(path=filename, mode="w") as io:
+        io.write(nwbfile)
+
+    return filename
---
+++ nwb2bids/test/test_reformat.py
@@ -0,0 +1,20 @@
+from nwb2bids import reformat
+import os
+
+def test_reformat_all(nwb_testdata, tmp_path):
+   nwb_testdir = os.path.dirname(nwb_testdata)
+   reformat.reformat_all(nwb_testdir, tmp_path)
+   # This should be done by invoking the validator once BEP032 is in the BIDS schema:
+   for root, dirs, files in os.walk(tmp_path):
+      if root == tmp_path:
+         assert dirs == ['sub-1234']
+         assert files == ['participants.json', 'participants.tsv']
+      if root == os.path.join(tmp_path, 'sub-1234'):
+         assert dirs == ['ses-20240309']
+         assert files == ['sessions.json', 'sessions.tsv']
+      if root == os.path.join(tmp_path, 'sub-1234', 'ses-20240309'):
+         assert dirs == ['ephys']
+         assert files == []
+      if root == os.path.join(tmp_path, 'sub-1234', 'ses-20240309', 'ephys'):
+         assert dirs == []
+         assert files == ['contacts.tsv', 'sub-1234_ses-20240309_ephys.nwb', 'probes.tsv', 'channels.tsv']
