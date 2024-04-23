44d7a704b7eb0d7e1168bb36df200d7c30424216
Horea Christian <chr@chymera.eu>
2024-04-22 23:37:29 -0400

    Added command line bindings

---
+++ nwb2bids/cli.py
@@ -0,0 +1,13 @@
+from clize import run
+from nwb2bids import base
+
+#def reposit():
+#   run(base.reposit)
+
+def main():
+   run({
+      'reposit': base.reposit,
+      })
+
+if __name__ == '__main__':
+	main()
