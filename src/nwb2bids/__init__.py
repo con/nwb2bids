2785cc9ed0dff4bcab8c1253c2c90a3f43120a1f
Cody Baker <cody.c.baker.phd@gmail.com>
2025-07-07 13:25:11 -0400

    chore: restore previous API with routing and deprecation warning

--- src/nwb2bids/__init__.py
+++ src/nwb2bids/__init__.py
@@ -7,10 +7,12 @@ Reorganize NWB files into a BIDS directory layout.
 
 from ._base import convert_nwb_dataset
 from ._base import DatasetConverter, SessionConverter
+from ._base._reposit import reposit
 
 __all__ = [
     # Public methods and classes
     "convert_nwb_dataset",
+    "reposit",  # TODO: remove
     "DatasetConverter",
     "SessionConverter",
     # Public submodules
