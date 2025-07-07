041dda8337b2338ccee8ab470f878667beb649d7
Cody Baker <cody.c.baker.phd@gmail.com>
2025-07-07 13:41:51 -0400

    chore: restore previous CLI with deprecation, swap to group structure for generality

--- src/nwb2bids/_base/_reposit.py
+++ src/nwb2bids/_base/_reposit.py
@@ -6,12 +6,11 @@ from ._convert_nwb_dataset import convert_nwb_dataset
 def reposit(
     in_dir: str, out_dir: str, *, no_copy: bool = False, additional_metadata_file_path: str | None = None
 ) -> None:
-    warnings.warn(
+    message = (
         "The `nwb2bids.reposit` function is deprecated and will be removed in a future release. "
         "Please use `nwb2bids.convert_nwb_dataset` instead.",
-        DeprecationWarning,
-        stacklevel=2,
     )
+    warnings.warn(message=message, category=DeprecationWarning, stacklevel=2)
 
     convert_nwb_dataset(
         nwb_directory=in_dir,
