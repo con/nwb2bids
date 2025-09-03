ae6410b0d205d4b39b28e8927fab0d190a15188e
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2025-09-03 08:14:44 -0400

    Update src/nwb2bids/bids_models/_model_globals.py
    Co-authored-by: Isaac To <candleindark@users.noreply.github.com>

--- src/nwb2bids/bids_models/_model_globals.py
+++ src/nwb2bids/bids_models/_model_globals.py
@@ -1,5 +1,21 @@
 _SPECIES_REGEX = r"[A-Za-z ]+|http://purl.obolibrary.org/obo/NCBITaxon_\d+"
-_ALLOWED_SEXES = [
+_ALLOWED_SEXES = {
+    "male",
+    "m",
+    "M",
+    "MALE",
+    "Male",
+    "female",
+    "f",
+    "F",
+    "FEMALE",
+    "Female",
+    "other",
+    "o",
+    "O",
+    "OTHER",
+    "Other",
+}
     "male",
     "m",
     "M",
