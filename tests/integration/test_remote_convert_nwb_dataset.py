d2d769a99b0a1a2e9d395f5e225131361761936d
Cody Baker <cody.c.baker.phd@gmail.com>
2025-09-15 10:52:00 -0400

    fix: fix remote test case

--- tests/integration/test_remote_convert_nwb_dataset.py
+++ tests/integration/test_remote_convert_nwb_dataset.py
@@ -6,34 +6,61 @@ import nwb2bids
 
 
 @pytest.mark.remote
-def test_remote_dataset_converter(temporary_bids_directory: pathlib.Path):
-    dataset_converter = nwb2bids.DatasetConverter.from_remote_dandiset(dandiset_id="000003", limit=3)
+def test_remote_convert_nwb_dataset(temporary_bids_directory: pathlib.Path):
+    dataset_converter = nwb2bids.DatasetConverter.from_remote_dandiset(dandiset_id="000003", limit=2)
     dataset_converter.extract_metadata()
     dataset_converter.convert_to_bids_dataset(bids_directory=temporary_bids_directory)
 
     expected_structure = {
         temporary_bids_directory: {
-            "directories": {"sub-123"},
+            "directories": {"sub-YutaMouse20"},
             "files": {"dataset_description.json", "participants.json", "participants.tsv"},
         },
         temporary_bids_directory
-        / "sub-123": {
-            "directories": {"ses-456"},
-            "files": {"sub-123_sessions.json", "sub-123_sessions.tsv"},
+        / "sub-YutaMouse20": {
+            "directories": {"ses-YutaMouse20-140321", "ses-YutaMouse20-140327"},
+            "files": {"sub-YutaMouse20_sessions.json", "sub-YutaMouse20_sessions.tsv"},
         },
         temporary_bids_directory
-        / "sub-123"
-        / "ses-456": {
+        / "sub-YutaMouse20"
+        / "ses-YutaMouse20-140321": {
             "directories": {"ecephys"},
             "files": set(),
         },
         temporary_bids_directory
-        / "sub-123"
-        / "ses-456"
+        / "sub-YutaMouse20"
+        / "ses-YutaMouse20-140327": {
+            "directories": {"ecephys"},
+            "files": set(),
+        },
+        temporary_bids_directory
+        / "sub-YutaMouse20"
+        / "ses-YutaMouse20-140321"
         / "ecephys": {
             "directories": set(),
             "files": {
-                "sub-123_ses-456_ecephys.nwb",
+                "sub-YutaMouse20_ses-YutaMouse20-140321_channels.json",
+                "sub-YutaMouse20_ses-YutaMouse20-140321_channels.tsv",
+                "sub-YutaMouse20_ses-YutaMouse20-140321_ecephys.nwb",
+                "sub-YutaMouse20_ses-YutaMouse20-140321_electrodes.json",
+                "sub-YutaMouse20_ses-YutaMouse20-140321_electrodes.tsv",
+                "sub-YutaMouse20_ses-YutaMouse20-140321_probes.json",
+                "sub-YutaMouse20_ses-YutaMouse20-140321_probes.tsv",
+            },
+        },
+        temporary_bids_directory
+        / "sub-YutaMouse20"
+        / "ses-YutaMouse20-140327"
+        / "ecephys": {
+            "directories": set(),
+            "files": {
+                "sub-YutaMouse20_ses-YutaMouse20-140327_channels.json",
+                "sub-YutaMouse20_ses-YutaMouse20-140327_channels.tsv",
+                "sub-YutaMouse20_ses-YutaMouse20-140327_ecephys.nwb",
+                "sub-YutaMouse20_ses-YutaMouse20-140327_electrodes.json",
+                "sub-YutaMouse20_ses-YutaMouse20-140327_electrodes.tsv",
+                "sub-YutaMouse20_ses-YutaMouse20-140327_probes.json",
+                "sub-YutaMouse20_ses-YutaMouse20-140327_probes.tsv",
             },
         },
     }
--- tests/unit/test_remote_dataset_converter.py
+++ tests/unit/test_remote_dataset_converter.py
@@ -1,9 +1,7 @@
 """Unit tests for the `DatasetConverter` class and its methods."""
 
-import json
 import pathlib
 
-import pandas
 import pydantic
 import pytest
 
@@ -12,28 +10,29 @@ import nwb2bids
 
 @pytest.mark.remote
 def test_remote_dataset_converter_initialization(temporary_bids_directory: pathlib.Path):
-    dataset_converter = nwb2bids.DatasetConverter.from_remote_dandiset(dandiset_id="000003")
+    dataset_converter = nwb2bids.DatasetConverter.from_remote_dandiset(dandiset_id="000003", limit=2)
     assert isinstance(dataset_converter, nwb2bids.DatasetConverter)
 
 
 @pytest.mark.remote
 def test_remote_dataset_converter_metadata_extraction(temporary_bids_directory: pathlib.Path):
-    dataset_converter = nwb2bids.DatasetConverter.from_remote_dandiset(dandiset_id="000003")
+    dataset_converter = nwb2bids.DatasetConverter.from_remote_dandiset(dandiset_id="000003", limit=2)
     dataset_converter.extract_metadata()
 
-    assert len(dataset_converter.session_converters) == 101
+    assert len(dataset_converter.session_converters) == 2
 
     session_converter = dataset_converter.session_converters[0]
-    assert session_converter.session_id == "YutaMouse44-151128"
+    assert session_converter.session_id == "YutaMouse20-140321"
     assert session_converter.nwbfile_paths == [
-        pydantic.HttpUrl("https://dandiarchive.s3.amazonaws.com/blobs/b9c/99c/b9c99cba-0f2b-469c-9747-22c69ac8170f")
+        pydantic.HttpUrl("https://dandiarchive.s3.amazonaws.com/blobs/bb8/1f7/bb81f7b3-4cfa-40e7-aa89-95beb1954d8c")
     ]
 
     session_metadata = session_converter.session_metadata
     assert session_metadata.participant == nwb2bids.bids_models.Participant(
-        participant_id="YutaMouse44", species="Mus musculus", sex=None, strain=None
+        participant_id="YutaMouse20", species="Mus musculus", sex=None, strain=None
     )
     assert session_metadata.events is None
+
     assert session_metadata.probe_table == nwb2bids.bids_models.ProbeTable(
         probes=[
             nwb2bids.bids_models.Probe(
@@ -41,113 +40,15 @@ def test_remote_dataset_converter_metadata_extraction(temporary_bids_directory: 
             )
         ]
     )
+
     assert session_metadata.channel_table is not None
-    assert session_metadata.channel_table[0] == 1
+    assert len(session_metadata.channel_table.channels) == 65
+    assert session_metadata.channel_table.channels[0] == nwb2bids.bids_models.Channel(
+        channel_id="0", electrode_id="0", type="EXT", unit="V", sampling_frequency=None, gain=None
+    )
+
     assert session_metadata.electrode_table is not None
-    assert session_metadata.electrode_table[0] == 1
-
-
-@pytest.mark.remote
-def test_remote_dataset_converter_write_dataset_description(temporary_bids_directory: pathlib.Path):
-    dataset_converter = nwb2bids.DatasetConverter.from_remote_dandiset(dandiset_id="000003")
-    dataset_converter.extract_metadata()
-    dataset_converter.write_dataset_description(bids_directory=temporary_bids_directory)
-
-    expected_structure = {temporary_bids_directory: {"directories": set(), "files": {"dataset_description.json"}}}
-    nwb2bids.testing.assert_subdirectory_structure(
-        directory=temporary_bids_directory, expected_structure=expected_structure
+    assert len(session_metadata.channel_table.channels) == 65
+    assert session_metadata.electrode_table.electrodes[0] == nwb2bids.bids_models.Electrode(
+        electrode_id=0, probe_id="Implant", location="unknown"
     )
-
-    dataset_description_file_path = temporary_bids_directory / "dataset_description.json"
-    with dataset_description_file_path.open(mode="r") as file_stream:
-        dataset_description_json = json.load(fp=file_stream)
-
-    expected_dataset_description = {
-        "Name": "test",
-        "Description": "TODO",
-        "BIDSVersion": "1.10",
-        "DatasetType": "raw",
-        "License": "CC-BY-4.0",
-        "Authors": ["Cody Baker", "Yaroslav Halchenko"],
-    }
-    assert dataset_description_json == expected_dataset_description
-
-
-@pytest.mark.remote
-def test_remote_dataset_converter_write_subject_metadata(temporary_bids_directory: pathlib.Path):
-    dataset_converter = nwb2bids.DatasetConverter.from_remote_dandiset(dandiset_id="000003")
-    dataset_converter.extract_metadata()
-    dataset_converter.write_participants_metadata(bids_directory=temporary_bids_directory)
-
-    expected_structure = {
-        temporary_bids_directory: {
-            "directories": set(),
-            "files": {"dataset_description.json", "participants.json", "participants.tsv"},
-        }
-    }
-    nwb2bids.testing.assert_subdirectory_structure(
-        directory=temporary_bids_directory, expected_structure=expected_structure
-    )
-
-    participants_tsv_file_path = temporary_bids_directory / "participants.tsv"
-    participants_data_frame = pandas.read_csv(filepath_or_buffer=participants_tsv_file_path, sep="\t", dtype=str)
-
-    expected_data_frame = pandas.DataFrame(
-        {"participant_id": ["sub-123"], "species": ["Mus musculus"], "sex": ["male"]}
-    )
-    pandas.testing.assert_frame_equal(left=participants_data_frame, right=expected_data_frame)
-
-    participants_json_file_path = temporary_bids_directory / "participants.json"
-    with participants_json_file_path.open(mode="r") as file_stream:
-        participants_json = json.load(fp=file_stream)
-    expected_participants_json = {
-        "participant_id": "A unique identifier for this participant.",
-        "species": (
-            "The species should be the proper Latin binomial species name from the NCBI Taxonomy "
-            "(for example, Mus musculus)."
-        ),
-        "sex": (
-            'String value indicating phenotypical sex, one of "male", "female", "other".\n'
-            '\tFor "male", use one of these values: male, m, M, MALE, Male.\n'
-            '\tFor "female", use one of these values: female, f, F, FEMALE, Female.\n'
-            '\tFor "other", use one of these values: other, o, O, OTHER, Other.'
-        ),
-    }
-    assert participants_json == expected_participants_json
-
-
-@pytest.mark.remote
-def test_remote_dataset_converter_write_sessions_metadata(temporary_bids_directory: pathlib.Path):
-    dataset_converter = nwb2bids.DatasetConverter.from_remote_dandiset(dandiset_id="000003")
-    dataset_converter.extract_metadata()
-    dataset_converter.write_sessions_metadata(bids_directory=temporary_bids_directory)
-
-    expected_structure = {
-        temporary_bids_directory: {"directories": {"sub-123"}, "files": {"dataset_description.json"}},
-        temporary_bids_directory
-        / "sub-123": {
-            "directories": {"ses-456"},
-            "files": {"sub-123_sessions.json", "sub-123_sessions.tsv"},
-        },
-        temporary_bids_directory
-        / "sub-123"
-        / "ses-456": {
-            "directories": set(),
-            "files": set(),
-        },
-    }
-    nwb2bids.testing.assert_subdirectory_structure(
-        directory=temporary_bids_directory, expected_structure=expected_structure
-    )
-
-    sessions_tsv_file_path = temporary_bids_directory / "sub-123" / "sub-123_sessions.tsv"
-    sessions_data_frame = pandas.read_csv(filepath_or_buffer=sessions_tsv_file_path, sep="\t", dtype=str)
-
-    expected_data_frame = pandas.DataFrame({"session_id": ["ses-456"]})
-    pandas.testing.assert_frame_equal(left=sessions_data_frame, right=expected_data_frame)
-
-    sessions_json_file_path = temporary_bids_directory / "sub-123" / "sub-123_sessions.json"
-    with sessions_json_file_path.open(mode="r") as file_stream:
-        sessions_json = json.load(fp=file_stream)
-    expected_sessions_json = {"session_id": "A unique session identifier."}
-    assert sessions_json == expected_sessions_json
