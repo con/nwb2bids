9001c474e4568ee356eff904e1869c2694dfab11
Austin Macdonald <austin@dartmouth.edu>
2025-09-24 12:01:13 -0500

    doc: add autogen API docs

---
+++ docs/source/api/bids_models.rst
@@ -0,0 +1,7 @@
+BIDS Models
+===========
+
+.. automodule:: nwb2bids.bids_models
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,21 @@
 """Configuration file for the Sphinx documentation builder."""
 
+import sys
+from pathlib import Path
+
+# Add source directory to path for autodoc
+sys.path.insert(0, str(Path(__file__).resolve().parents[2] / "src"))
+
 project = "nwb2bids"
 copyright = "2025, Cody Baker"
 author = "Cody Baker"
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
 
 exclude_patterns = []
 
@@ -22,3 +33,21 @@ html_context = {
     "github_version": "main",
     "doc_path": "docs/source",
 }
+
+# Napoleon settings
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_use_param = False
+napoleon_use_ivar = True
+
+# Autodoc settings
+autoclass_content = "both"
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "private-members": False,
+    "undoc-members": True,
+    "show-inheritance": False,
+    "exclude-members": "__new__",
+}
+add_module_names = False
