fd8672194c42a5c4aaa03d4794f92defbf57b0c6
Cody Baker <cody.c.baker.phd@gmail.com>
2025-10-01 13:04:32 -0400

    feat: suppress meaningless PyNWB warnings

---
+++ src/nwb2bids/_tools/_cache_nwb.py
@@ -0,0 +1,18 @@
+import functools
+import pathlib
+import warnings
+
+import pynwb
+
+warnings.filterwarnings(action="ignore", message="No cached namespaces found in .*")
+warnings.filterwarnings(action="ignore", message="Ignoring cached namespace .*")
+warnings.filterwarnings(action="ignore", message="Stimulus description 'NA' for .*")
+warnings.filterwarnings(action="ignore", message="Use of icephys_filtering is deprecated and will be removed .*")
+
+
+@functools.cache
+def cache_read_nwb(file_path: pathlib.Path) -> pynwb.NWBFile:
+    """Cache the read operation per NWB file path to speed up repeated calls."""
+
+    nwbfile = pynwb.read_nwb(path=file_path)
+    return nwbfile
