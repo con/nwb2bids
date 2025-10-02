2d89cfcc07b6fd1fae0466e4610d10accfc5b6e6
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2025-10-02 17:02:46 -0400

    PR suggestions

--- src/nwb2bids/_tools/_cache_nwb.py
+++ src/nwb2bids/_tools/_cache_nwb.py
@@ -4,10 +4,10 @@ import warnings
 
 import pynwb
 
-warnings.filterwarnings(action="ignore", message="No cached namespaces found in .*")
-warnings.filterwarnings(action="ignore", message="Ignoring cached namespace .*")
-warnings.filterwarnings(action="ignore", message="Stimulus description 'NA' for .*")
-warnings.filterwarnings(action="ignore", message="Use of icephys_filtering is deprecated and will be removed .*")
+warnings.filterwarnings(action="ignore", message="No cached namespaces found in .+")
+warnings.filterwarnings(action="ignore", message="Ignoring cached namespace .+")
+warnings.filterwarnings(action="ignore", message="Stimulus description 'NA' for .+")
+warnings.filterwarnings(action="ignore", message="Use of icephys_filtering is deprecated and will be removed .+")
 
 
 @functools.cache
