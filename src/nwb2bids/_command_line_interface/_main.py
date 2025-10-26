f05d58df661578c4097eaf49860036a2824f508c
Cody Baker <cody.c.baker.phd@gmail.com>
2025-10-26 00:31:44 -0400

    chore: add back blurb for consistency

--- src/nwb2bids/_command_line_interface/_main.py
+++ src/nwb2bids/_command_line_interface/_main.py
@@ -53,7 +53,7 @@ def _nwb2bids_cli():
     help=(
         "On each unique run of nwb2bids, a run ID is generated. "
         "Set this option to override this to any identifying string. "
-        "This ID is used in the naming of the notification reports saved to your cache directory. "
+        "This ID is used in the naming of the notification and sanitization reports saved to your cache directory. "
         'The default ID uses runtime timestamp information of the form "date-%Y%m%d_time-%H%M%S."'
     ),
     required=False,
