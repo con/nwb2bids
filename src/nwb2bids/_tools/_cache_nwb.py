1553a3a83ef792dd264cf8501fbf6636e45f6144
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2025-11-05 13:08:58 -0500

    Apply suggestion from @CodyCBakerPhD

--- src/nwb2bids/_tools/_cache_nwb.py
+++ src/nwb2bids/_tools/_cache_nwb.py
@@ -19,6 +19,7 @@ for message in ignored_pynwb_message_patterns:
 @functools.cache
 def cache_read_nwb(file_path: pathlib.Path) -> pynwb.NWBFile:
     """Cache the read operation per NWB file path to speed up repeated calls."""
-    resolved_path = file_path.resolve()  # Always resolve in case of symlinks (i.e., DataLad most likely)
+     # Always resolve in case of symlinks, mostly relevant to Windows (i.e., from DataLad most likely)
+    resolved_path = file_path.resolve()
     nwbfile = pynwb.read_nwb(path=resolved_path)
     return nwbfile
