49dc2de32dc3d3d70a0f661a14cab1488be460ec
Cody Baker <cody.c.baker.phd@gmail.com>
2025-11-07 14:57:54 -0500

    fix: tests

--- src/nwb2bids/_command_line_interface/_main.py
+++ src/nwb2bids/_command_line_interface/_main.py
@@ -82,7 +82,9 @@ def _run_convert_nwb_dataset(
         console_notification = rich_click.style(text=text, fg="yellow")
         rich_click.echo(message=console_notification)
 
-    not_any_failures = not messages or not any(message.severity == Severity.ERROR for message in messages)
+    not_any_failures = not notifications or not any(
+        notification.severity == Severity.ERROR for notification in notifications
+    )
     if not_any_failures and not silent:
         text = "\nBIDS dataset was successfully created!\n\n"
         console_notification = rich_click.style(text=text, fg="green")
