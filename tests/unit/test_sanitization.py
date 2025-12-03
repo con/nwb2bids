f8dd2bafe9f3e910bda66b7b1a5880d80a93b8c2
Isaac To <isaac.chun.to@gmail.com>
2025-12-03 14:44:17 -0800

    test(fix): test `sanitize_session_id()` in `test_sanitize_session_id()`
    `sanitize_participant_id()` instead of `sanitize_session_id()`
     was tested in `test_sanitize_session_id()`, which I don't
     think was intended.

--- tests/unit/test_sanitization.py
+++ tests/unit/test_sanitization.py
@@ -28,23 +28,21 @@ def test_sanitize_session_id():
 
     # TODO: think about if subject info in session IDs deserves special cleaning
     assert (
-        nwb2bids.sanitization.sanitize_participant_id(
-            participant_id="Session 12 subject 5", sanitization_level=sanitization_level
+        nwb2bids.sanitization.sanitize_session_id(
+            session_id="Session 12 subject 5", sanitization_level=sanitization_level
         )
         == "Session+12+subject+5"
     )
 
     # TODO: think about if timestamps deserve special cleaning
     assert (
-        nwb2bids.sanitization.sanitize_participant_id(
-            participant_id="2025-02-03T01:08:12.1236", sanitization_level=sanitization_level
+        nwb2bids.sanitization.sanitize_session_id(
+            session_id="2025-02-03T01:08:12.1236", sanitization_level=sanitization_level
         )
         == "2025+02+03T01+08+12+1236"
     )
 
     assert (
-        nwb2bids.sanitization.sanitize_participant_id(
-            participant_id="12_02_2025", sanitization_level=sanitization_level
-        )
+        nwb2bids.sanitization.sanitize_session_id(session_id="12_02_2025", sanitization_level=sanitization_level)
         == "12+02+2025"
     )
