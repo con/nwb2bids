0e67189747e8ae75d89803a3c3c50be3fbcfbac6
Isaac To <isaac.chun.to@gmail.com>
2025-12-03 14:37:14 -0800

    refactor(test): reduce code in `test_sanitization.py`
    By using the parametrization feature from pytest

--- tests/unit/test_sanitization.py
+++ tests/unit/test_sanitization.py
@@ -5,44 +5,43 @@ The regular expression for labels is trusted to work over all basic cases.
 Any edge cases may also be added here to ensure confidence of coverage.
 """
 
+import pytest
+
 import nwb2bids
 
 
-def test_sanitize_participant_id():
+@pytest.mark.parametrize(
+    "participant_id, expected",
+    [
+        ("Mouse 12", "Mouse+12"),
+        ("Raw/data?sub-01@today", "Raw+data+sub+01+today"),
+    ],
+)
+def test_sanitize_participant_id(participant_id, expected):
     sanitization_level = nwb2bids.sanitization.SanitizationLevel.CRITICAL_BIDS_LABELS
 
-    assert (
-        nwb2bids.sanitization.sanitize_participant_id(participant_id="Mouse 12", sanitization_level=sanitization_level)
-        == "Mouse+12"
-    )
     assert (
         nwb2bids.sanitization.sanitize_participant_id(
-            participant_id="Raw/data?sub-01@today", sanitization_level=sanitization_level
+            participant_id=participant_id, sanitization_level=sanitization_level
         )
-        == "Raw+data+sub+01+today"
+        == expected
     )
 
 
-def test_sanitize_session_id():
+@pytest.mark.parametrize(
+    "session_id, expected",
+    [
+        # TODO: think about if subject info in session IDs deserves special cleaning
+        ("Session 12 subject 5", "Session+12+subject+5"),
+        # TODO: think about if timestamps deserve special cleaning
+        ("2025-02-03T01:08:12.1236", "2025+02+03T01+08+12+1236"),
+        ("12_02_2025", "12+02+2025"),
+    ],
+)
+def test_sanitize_session_id(session_id, expected):
     sanitization_level = nwb2bids.sanitization.SanitizationLevel.CRITICAL_BIDS_LABELS
 
-    # TODO: think about if subject info in session IDs deserves special cleaning
     assert (
-        nwb2bids.sanitization.sanitize_session_id(
-            session_id="Session 12 subject 5", sanitization_level=sanitization_level
-        )
-        == "Session+12+subject+5"
-    )
-
-    # TODO: think about if timestamps deserve special cleaning
-    assert (
-        nwb2bids.sanitization.sanitize_session_id(
-            session_id="2025-02-03T01:08:12.1236", sanitization_level=sanitization_level
-        )
-        == "2025+02+03T01+08+12+1236"
-    )
-
-    assert (
-        nwb2bids.sanitization.sanitize_session_id(session_id="12_02_2025", sanitization_level=sanitization_level)
-        == "12+02+2025"
+        nwb2bids.sanitization.sanitize_session_id(session_id=session_id, sanitization_level=sanitization_level)
+        == expected
     )
