f5fcf15589c622a3c6c725e8d0eb97584fb23137
Isaac To <candleindark@users.noreply.github.com>
2025-12-04 08:59:37 -0800

    Enhance tests for sanitization and reduce code duplications in those tests (#210)
    * test(fix): test `sanitize_session_id()` in `test_sanitize_session_id()`
    
    `sanitize_participant_id()` instead of `sanitize_session_id()`
     was tested in `test_sanitize_session_id()`, which I don't
     think was intended.
    
    * refactor(test): reduce code in `test_sanitization.py`
    
    By using the parametrization feature from pytest
    
    * test: parametrize `sanitization_level`
    
    In `test_sanitize_participant_id()` and
    `test_sanitize_session_id()`
    
    * style(test): simplify calls to sanitization func with positional args
    
    There is no benefits from using
    keyword arguments since the
    input variable names are the same
    as the keyword arguments' name
    
    * docs: complete docstring for `sanitize_session_id()`
    
    * docs: complete docstring for `sanitize_participant_id()`
    
    * test: add test cases for sanitization level set to `SanitizationLevel.NONE`
    
    ---------
    
    Co-authored-by: Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>

--- src/nwb2bids/sanitization/_participant.py
+++ src/nwb2bids/sanitization/_participant.py
@@ -19,16 +19,18 @@ def sanitize_participant_id(
     ----------
     participant_id : str
         The original subject ID to be sanitized.
-
-    Returns
-    -------
-    sanitized_participant_id : str
-        The sanitized participant ID label (without the 'sub-' entity prefix).
+    sanitization_level : SanitizationLevel, optional
+        The level of sanitization to apply. Default is `SanitizationLevel.NONE`.
     sanitization_file_path : file path, optional
         The path to a file where a report of the sanitization actions taken will be written.
         If None, no report is written.
     sanitization_report_context : str, optional
         Additional context to include in the sanitization report (e.g., the name or scope of the calling method).
+
+    Returns
+    -------
+    sanitized_participant_id : str
+        The sanitized participant ID label (without the 'sub-' entity prefix).
     """
     if sanitization_level > SanitizationLevel.NONE:
         sanitized_participant_id = _sanitize_label(label=participant_id)
