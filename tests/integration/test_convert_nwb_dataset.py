926ad35870783a584be916bc4ba50dd53b5ad21d
Cody Baker <cody.c.baker.phd@gmail.com>
2025-12-07 23:52:02 -0500

    updated writing practices and test cases

--- tests/integration/test_convert_nwb_dataset.py
+++ tests/integration/test_convert_nwb_dataset.py
@@ -3,8 +3,6 @@
 import json
 import pathlib
 
-import numpy
-import pandas
 import pytest
 
 import nwb2bids
@@ -135,16 +133,9 @@ def test_ecephys_convert_nwb_dataset(ecephys_nwbfile_path: pathlib.Path, tempora
     )
 
     probes_tsv_file_path = temporary_bids_directory / "sub-123" / "ses-789" / "ecephys" / "sub-123_ses-789_probes.tsv"
-    probes_tsv_dataframe = pandas.read_csv(filepath_or_buffer=probes_tsv_file_path, sep="\t")
-    expected_probes_tsv_dataframe = pandas.DataFrame(
-        data={
-            "probe_id": {0: "Device"},
-            "type": {0: numpy.nan},
-            "description": {0: "description"},
-            "manufacturer": {0: numpy.nan},
-        }
-    )
-    pandas.testing.assert_frame_equal(left=probes_tsv_dataframe, right=expected_probes_tsv_dataframe)
+    probes_tsv_lines = probes_tsv_file_path.read_text().splitlines()
+    expected_lines = ["probe_name\ttype\tdescription", "Device\tN/A\tdescription"]
+    assert probes_tsv_lines == expected_lines
 
 
 def test_implicit_bids_directory(
