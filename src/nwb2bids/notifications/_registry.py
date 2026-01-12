6be0725d0e709b79b1a0abceef187a8438aaf673
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2026-01-12 12:39:03 -0500

    Refactor notification handling with proposals

--- src/nwb2bids/notifications/_registry.py
+++ src/nwb2bids/notifications/_registry.py
@@ -5,6 +5,7 @@ import typing_extensions
 
 from ._notification import Category, DataStandard, Notification, Severity
 
+# Proposal 0: Current approach - same class, different instances
 a = Notification(
     title="Missing participant ID",
     reason="A unique ID is required for all individual participants.",
@@ -16,8 +17,13 @@ a = Notification(
     severity=Severity.CRITICAL,
 )
 
+registry: dict[str, Notification] = {notification.title: notification for notification in [a]}
+# Note how the title currently has spaces; shorter codes/aliases could be added for easier lookup in the dictionary
 
-class MissingParticipantID(Notification):
+
+
+# Proposal 1: Child classes with mostly hard-coded fields followed by mutation
+class MissingParticipantID1(Notification):
     title = "Missing participant ID"
     reason = "A unique ID is required for all individual participants."
     solution = "Specify the `subject_id` field of the Subject object attached to the NWB file."
@@ -27,10 +33,13 @@ class MissingParticipantID(Notification):
     severity = Severity.CRITICAL
 
 
-b = MissingParticipantID
-b.source_file_paths = "Test2"
+b = MissingParticipantID1
+b.source_file_paths = ["Proposal1.nwb"]
+
+registry: dict[str, Notification] = {notification.__name__: notification for notification in [b]}
 
 
+# Proposal 2: Child classes with mostly internal hard-coded values followed by class method instantiation for dynamic parts
 class MissingParticipantID2(Notification):
     @classmethod
     def from_paths(cls, file_paths: list[pathlib.Path] | list[pydantic.HttpUrl]) -> typing_extensions.Self:
@@ -46,5 +55,29 @@ class MissingParticipantID2(Notification):
         )
         return instance
 
+c = MissingParticipantID2.from_paths(file_paths=["Proposal2.nwb"])
 
-c = MissingParticipantID.from_paths(file_paths=["Test3"])
+registry: dict[str, Notification] = {notification.__name__: notification for notification in [c]}
+
+
+
+# Proposal 3: Factory class
+_internal_hardcoded_values: dict[str, dict[str, str]] = {
+    "MissingParticipantID": {
+        "title": "Missing participant ID",
+        "reason": "A unique ID is required for all individual participants.",
+        ...etc
+    },
+    # ... more types
+}
+# NOTE: this dictionary could/should be stored outside the Python in some JSON or YAML file
+
+class Notification:
+    @classmethod
+    def from_paths(cls, notification_type: str, file_paths: list):
+        return cls(**NOTIFICATIONS[notification_type], notification_type=notification_type, source_file_paths=file_paths)
+
+d = Notification.from_paths(notification_type=MissingParticipantID, file_paths=["Proposal3.nwb"])
+
+registry: dict[str, Notification] = {notification.notification_type: notification for notification in [d]}
+
