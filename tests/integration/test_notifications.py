7c83f2f59ede30f865c4d1c4da5ee76142e86c79
Cody Baker <cody.c.baker.phd@gmail.com>
2026-01-21 16:47:23 -0500

    debug tests

--- tests/integration/test_notifications.py
+++ tests/integration/test_notifications.py
@@ -20,62 +20,43 @@ def test_notifications_1(problematic_nwbfile_path_1: pathlib.Path, temporary_bid
 
     notifications = dataset_converter.notifications
     expected_notifications = [
-        {
-            "category": "SCHEMA_INVALIDATION",
-            "data_standards": ["DANDI"],
-            "examples": [],
-            "field": "nwbfile.subject.species",
-            "notification_id": None,
-            "reason": "Participant species is not a proper Latin binomial or NCBI " "Taxonomy id.",
-            "severity": "ERROR",
-            "solution": "Rename the subject species to a Latin binomial, obolibrary "
-            "taxonomy link, or NCBI taxonomy reference.",
-            "source_file_paths": nwb_paths,
-            "target_file_paths": None,
-            "title": "Invalid species",
-        },
-        {
-            "category": "STYLE_SUGGESTION",
-            "data_standards": ["BIDS", "DANDI"],
-            "examples": ["`ab_01` -> `ab+01`", "`subject #2` -> `subject+2`", "`id 2 from 9/1/25` -> `id+2+9+1+25`"],
-            "field": "nwbfile.subject.subject_id",
-            "notification_id": "InvalidParticipantID",
-            "reason": "The participant ID contains invalid characters. BIDS allows only "
-            "the plus sign to be used as a separator in the subject entity "
-            "label. Underscores, dashes, spaces, slashes, and other special "
-            "characters (including #) are expressly forbidden.",
-            "severity": "ERROR",
-            "solution": "Rename the subject without using any special characters except " "for `+`.",
-            "source_file_paths": nwb_paths,
-            "target_file_paths": None,
-            "title": "Invalid participant ID",
-        },
-        {
-            "category": "STYLE_SUGGESTION",
-            "data_standards": ["BIDS"],
-            "examples": ["`male` -> `M`", "`Female` -> `F`", "`n/a` -> `U`", "`hermaphrodite` -> `O`"],
-            "field": "nwbfile.subject.sex",
-            "notification_id": None,
-            "reason": "Participant sex is not one of the allowed patterns by BIDS.",
-            "severity": "ERROR",
-            "solution": "Rename the subject sex to be one of the accepted values.",
-            "source_file_paths": nwb_paths,
-            "target_file_paths": None,
-            "title": "Invalid participant sex (BIDS)",
-        },
-        {
-            "category": "STYLE_SUGGESTION",
-            "data_standards": ["DANDI"],
-            "examples": ["`male` -> `M`", "`Female` -> `F`", "`n/a` -> `U`", "`hermaphrodite` -> `O`"],
-            "field": "nwbfile.subject.sex",
-            "notification_id": None,
-            "reason": "Participant sex is not one of the allowed patterns by the common " "archives.",
-            "severity": "ERROR",
-            "solution": "Rename the subject sex to be one of the accepted values.",
-            "source_file_paths": nwb_paths,
-            "target_file_paths": None,
-            "title": "Invalid participant sex (archives)",
-        },
+        nwb2bids.Notification(
+            title="Invalid species",
+            reason="Participant species is not a proper Latin binomial or NCBI Taxonomy id.",
+            solution=(
+                "Rename the subject species to a Latin binomial, obolibrary taxonomy link, or NCBI taxonomy "
+                "reference."
+            ),
+            examples=[],
+            field="nwbfile.subject.species",
+            source_file_paths=nwb_paths,
+            data_standards=[nwb2bids.notifications.DataStandard.DANDI],
+            category=nwb2bids.notifications.Category.SCHEMA_INVALIDATION,
+            severity=nwb2bids.notifications.Severity.ERROR,
+        ),
+        nwb2bids.Notification.from_definition(notification_id="InvalidParticipantID", source_file_paths=nwb_paths),
+        nwb2bids.Notification(
+            title="Invalid participant sex (BIDS)",
+            reason="Participant sex is not one of the allowed patterns by BIDS.",
+            solution="Rename the subject sex to be one of the accepted values.",
+            examples=["`male` -> `M`", "`Female` -> `F`", "`n/a` -> `U`", "`hermaphrodite` -> `O`"],
+            field="nwbfile.subject.sex",
+            source_file_paths=nwb_paths,
+            data_standards=[nwb2bids.notifications.DataStandard.BIDS],
+            category=nwb2bids.notifications.Category.STYLE_SUGGESTION,
+            severity=nwb2bids.notifications.Severity.ERROR,
+        ),
+        nwb2bids.Notification(
+            title="Invalid participant sex (archives)",
+            reason="Participant sex is not one of the allowed patterns by the common archives.",
+            solution="Rename the subject sex to be one of the accepted values.",
+            examples=["`male` -> `M`", "`Female` -> `F`", "`n/a` -> `U`", "`hermaphrodite` -> `O`"],
+            field="nwbfile.subject.sex",
+            source_file_paths=nwb_paths,
+            data_standards=[nwb2bids.notifications.DataStandard.DANDI],
+            category=nwb2bids.notifications.Category.STYLE_SUGGESTION,
+            severity=nwb2bids.notifications.Severity.ERROR,
+        ),
     ]
     assert notifications == expected_notifications
 
