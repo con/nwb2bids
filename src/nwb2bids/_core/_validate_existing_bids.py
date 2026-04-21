911db73c3e3d29947eab3e5ebbe54284e3663634
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2026-04-21 01:29:30 -0400

    add fallback
    Add check for non-empty directory before validation.

--- src/nwb2bids/_core/_validate_existing_bids.py
+++ src/nwb2bids/_core/_validate_existing_bids.py
@@ -34,7 +34,10 @@ def _validate_bids_directory(path: pathlib.Path) -> pathlib.Path:
     if not path.parent.is_dir():
         raise ValueError(f"The parent path ({path.parent}) is not a directory.")
 
-    return _validate_existing_directory_as_bids(path)
+    if len(path.iterdir()) > 0:
+        return _validate_existing_directory_as_bids(path)
+
+    return path
 
 
 def _validate_existing_directory_as_bids(directory: pathlib.Path) -> pathlib.Path:
